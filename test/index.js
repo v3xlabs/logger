50e5a167f50803491b8ee75b556548d52279c0a5
Lucemans <luc@lucemans.nl>
2021-12-06 01:11:38 +0100

    Introduce Multi-line + Prettierrc

--- test/index.js
+++ test/index.js
@@ -1,16 +1,26 @@
 import { createLogger } from "../lib/index.js";
-import chalk from 'chalk';
+import chalk from "chalk";
 
 (async () => {
+    const log = createLogger(
+        {
+            ok: chalk.greenBright`[OK]`,
+            debug: chalk.magentaBright`[DEBUG]`,
+            info: {
+                label: chalk.cyan`[INFO]`,
+                newLine: chalk.cyan`⮡`,
+                newLineEnd: chalk.cyan`⮡`,
+            },
+            veryBigNetworkError: chalk.bgRed.white.bold`[NETWORK]`,
+        },
+        { padding: "PREPEND", newLine: '| ', newLineEnd: '\\-'},
+        console.log
+    );
 
-    const log = createLogger({
-        ok: 'OK',
-        debug: chalk.magenta('DEBUG'),
-        info: chalk.cyan('INFO')
-    }, 'PREPEND', '|', console.log);
-    
-    
-    log.ok('this is ok');
-    log.info('this is info');
-    log.debug('this is debug');
+    log.ok("this is ok");
+    log.info("this is info");
+    log.debug("this is debug");
+    log.veryBigNetworkError`Error 404 at -> Thing`;
+    log.debug("item 1", "item 2", "item 3", "item 4", "item 5");
+    log.info("item 1", "item 2", "item 3", "item 4", "item 5");
 })();
