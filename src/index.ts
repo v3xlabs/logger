38bbc65f573b26651d97c6bbfcd57dbde6fecbcd
Luc <luc@lucemans.nl>
2021-12-11 23:08:05 +0100

    Introduce zero-dependency mode! (#23)
    * Deprecate stripAnsi as a dependency
    
    * Update Licensing to reflect ansi-regex
    
    * Remove unused file
    
    * Update tests to match zero-deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-import stripAnsi from "strip-ansi";
 import { inspect } from "util";
-import { LogConfig, LogFunction, MethodConfig } from "./types";
+
+const ansi = new RegExp(
+    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)|(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))",
+    "g"
+);
+
+export const stripAnsi = (input: string) => input.replace(ansi, "");
+
+export type LogFunc = (
+    ...input: (string | object | object[] | unknown)[]
+) => void;
+
+export type LogFunction<K extends string> = {
+    [a in K]: LogFunc;
+};
+
+export type PadType = "PREPEND" | "APPEND";
+
+export type LogConfig = {
+    padding: PadType;
+    divider: string;
+    newLine: string;
+    newLineEnd: string;
+    color: boolean;
+};
+
+export type MethodConfig = {
+    label: string;
+    newLine?: string;
+    newLineEnd?: string;
+};
 
 export const createLogger = <A extends string>(
     methods: { [k in A]: string | MethodConfig },
@@ -14,7 +43,7 @@ export const createLogger = <A extends string>(
             newLine: "├-",
             newLineEnd: "└-",
             padding: "PREPEND",
-            color: true
+            color: true,
         },
         ...config,
     };
@@ -27,35 +56,36 @@ export const createLogger = <A extends string>(
     };
 
     // Convert all string methods to MethodConfig
-    const completeMethods: { [k in A]: Required<MethodConfig> } =
-        Object.assign(
-            {},
-            ...(Object.keys(methods) as A[]).map((a) => {
-                if (typeof methods[a] == "string") {
-                    // Return an inferred MethodConfig
-                    return {
-                        [a]: {
-                            ...inferredMethodConfig,
-                            ...{
-                                label: methods[a],
-                            },
-                        },
-                    };
-                }
-
-                // Return the MethodConfig that was provided
+    const completeMethods: { [k in A]: Required<MethodConfig> } = Object.assign(
+        {},
+        ...(Object.keys(methods) as A[]).map((a) => {
+            if (typeof methods[a] == "string") {
+                // Return an inferred MethodConfig
                 return {
                     [a]: {
                         ...inferredMethodConfig,
-                        ...(methods[a] as MethodConfig),
+                        ...{
+                            label: methods[a],
+                        },
                     },
                 };
-            })
-        );
+            }
+
+            // Return the MethodConfig that was provided
+            return {
+                [a]: {
+                    ...inferredMethodConfig,
+                    ...(methods[a] as MethodConfig),
+                },
+            };
+        })
+    );
 
     // Calculate the max length
     const maxLength = Math.max(
-        ...Object.values(completeMethods).map((a) => stripAnsi((a as Required<MethodConfig>).label).length)
+        ...Object.values(completeMethods).map(
+            (a) => stripAnsi((a as Required<MethodConfig>).label).length
+        )
     );
 
     return Object.assign(
@@ -88,23 +118,32 @@ export const createLogger = <A extends string>(
 
             return {
                 [methodHandle]: (...s: unknown[]) => {
-                    func(s.map(value => {
-                        if (typeof value !== "string") {
-                            value = inspect(value, false, 3, completeConfig.color);
-                        }
-                        return value;
-                    }).join("\n")
-                        .split("\n")
-                        .map((value, index, array) =>
-                            (index == 0
-                                ? paddedText + completeConfig.divider
-                                : (array.length - 1 == index
-                                        ? paddedPreEnd
-                                        : newLinePadding) +
-                                    completeConfig.divider) + value
-                        )
-                        .join("\n")
-                    )
+                    func(
+                        s
+                            .map((value) => {
+                                if (typeof value !== "string") {
+                                    value = inspect(
+                                        value,
+                                        false,
+                                        3,
+                                        completeConfig.color
+                                    );
+                                }
+                                return value;
+                            })
+                            .join("\n")
+                            .split("\n")
+                            .map(
+                                (value, index, array) =>
+                                    (index == 0
+                                        ? paddedText + completeConfig.divider
+                                        : (array.length - 1 == index
+                                              ? paddedPreEnd
+                                              : newLinePadding) +
+                                          completeConfig.divider) + value
+                            )
+                            .join("\n")
+                    );
                 },
             };
         })
