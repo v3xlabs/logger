cc15d39a61d8030c4dc8969b89fd997879da8cff
Antony <antoniostignjedec@gmail.com>
2021-12-11 22:29:41 +0100

    Introduce Jest Testing (#21)
    * Basic Jest setup & changes to generics
    
    * Introduce Testing CI/CD
    
    * Remove caching from CI/CD
    
    * Update stdout color detection
    
    * Update CI/CD to PR only
    
    * Introduce 100% Code Coverage
    
    * Force Coverage Report Generation
    
    * Remove Automatic color detection
    
    Co-authored-by: Lucemans <luc@lucemans.nl>

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
+    transform: {
+        ".ts": "ts-jest"
+    },
+    testMatch: ["<rootDir>/tests/**/*.test.[jt]s"]
+}
+
+export default config;
