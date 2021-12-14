d92ed94f1ba0573735cca973e08e2a607b084775
Jakob Helgesson <jakob.helgesson@gmail.com>
2021-12-14 15:20:57 +0100

    Configurable Divider on Method-Level  (#34)

--- tests/logger.test.ts
+++ tests/logger.test.ts
@@ -165,3 +165,49 @@ describe("Default values", () => {
         );
     });
 });
+
+describe("Custom divider", () => {
+    let logger: Logger<"foo" | "bar">;
+
+    beforeAll(() => {
+        logger = createLogger(
+            {
+                foo: {
+                    label: chalk.greenBright`[FOO]`,
+                    newLine: "| ",
+                    newLineEnd: `\\-`,
+                },
+                bar: {
+                    label: chalk.redBright`[BAR]`,
+                    newLine: "| ",
+                    newLineEnd: `\\-`,
+                    divider: " | ",
+                },
+            },
+            { padding: "PREPEND", color: false },
+            logFn
+        );
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it("should use default divider", () => {
+        logger.foo(
+            "This is a cool logging library",
+        );
+        expect(logFn).toBeCalledWith(
+            `${chalk.greenBright`[FOO]`} This is a cool logging library`
+        );
+    });
+
+    it("should use custom divider", () => {
+        logger.bar(
+            "This is even cooler",
+        );
+        expect(logFn).toBeCalledWith(
+            `${chalk.redBright`[BAR]`} | This is even cooler`
+        );
+    });
+});
