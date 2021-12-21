90f9c5fb84c0fd9b76f94bb48e7cdca2c0291298
Luc <luc@lucemans.nl>
2021-12-21 16:57:33 +0100

    Improve PR CI, Seperate Ansi (#58)
    * Improve PR CI, Seperate Ansi
    
    * Introduce ESLint
    
    * Introduce eslint-mistake to test ci
    
    * Fix ESLint Error
    
    Co-authored-by: prokopschield <prokop.schield@protonmail.com>
    
    Co-authored-by: prokopschield <prokop.schield@protonmail.com>

--- src/FileLog.ts
+++ src/FileLog.ts
@@ -1,16 +1,18 @@
-import { createWriteStream, existsSync, mkdirSync } from "fs";
-import { join, resolve } from "path";
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
+import { join, resolve } from 'path';
+
+import { stripAnsi } from './ansi';
 
 export type FileLoggerConfig = FileLoggerConfigNew | FileLoggerConfigAppend;
 
 type FileLoggerConfigNew = {
-    mode: "NEW_FILE";
+    mode: 'NEW_FILE';
     path: string;
     namePattern: string;
 };
 
 type FileLoggerConfigAppend = {
-    mode: "APPEND_FILE";
+    mode: 'APPEND_FILE';
     path: string;
 };
 
@@ -18,26 +20,27 @@ export const fileLogger = (config: FileLoggerConfig) => {
     let path = config.path;
 
     // If we are creating a new file generate the file name
-    if (config.mode == "NEW_FILE") {
+    if (config.mode == 'NEW_FILE') {
         let calculatedPattern = config.namePattern;
 
         path = join(config.path, calculatedPattern);
     }
 
     // Ensure the parent folder exists
-    const folder = resolve(path, "..");
+    const folder = resolve(path, '..');
+
     if (!existsSync(folder)) {
         mkdirSync(folder, { recursive: true });
     }
 
     // Create the stream to write out to
     const stream = createWriteStream(path, {
-        encoding: "utf-8",
+        encoding: 'utf-8',
         autoClose: true,
     });
 
     // Return the executable logging function
     return (input: string) => {
-        stream.write(input + "\n");
+        stream.write(stripAnsi(input) + '\n');
     };
 };
